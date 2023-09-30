3a103fbb700c75385fd3870fbed7c6bee492ca4d
조창욱 <jochang-uk@jochang-ug-ui-MacBookAir.local>
2023-09-30 17:41:41 +0900

    data types, nestedroute

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "theme";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,9 +13,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     {/* themeprovider로 감싸서 app에서 theme을 접근할 수 있게된다 */}
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </>
 );
 
