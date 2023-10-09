b30029e72f9d5dcec62d267418a8e894e47984c9
sheepdog13 <mikecho1234@naver.com>
2023-10-09 15:38:54 +0900

    react query isloding

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { useQuery } from "react-query";
 import styled from "styled-components";
@@ -43,19 +44,64 @@ const ListTitleBox = styled.div`
   }
 `;
 
+const CoinListBox = styled.div`
+  margin-top: 4px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 4px;
+`;
+const Coin = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 150px;
+  padding: 20px;
+  background-color: rgb(26, 32, 53);
+  color: white;
+  gap: 12px;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
 function Coins() {
   const { isLoading, data } = useQuery<Icoin[]>("allCoins", fetchCoins);
-  const aaa = false;
   return (
     <Wrap>
-      <SideComp />
-      <ContentBox>
-        <WelcomComp />
-        <ListTitleBox>
-          <span>Coin List</span>
-        </ListTitleBox>
-        <CoinListComp />
-      </ContentBox>
+      {isLoading ? (
+        <>LodingComp만들기</>
+      ) : (
+        <>
+          <SideComp />
+          <ContentBox>
+            <WelcomComp />
+            <ListTitleBox>
+              <span>Coin List</span>
+            </ListTitleBox>
+            <CoinListBox>
+              {data?.slice(0, 100).map((coin) => (
+                <Coin key={coin.id}>
+                  <Link
+                    to={{
+                      pathname: `/${coin.id}`,
+                      state: { name: coin.name },
+                    }}
+                  >
+                    <Img
+                      src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                    />
+                    {coin.name} &rarr;
+                  </Link>
+                </Coin>
+              ))}
+            </CoinListBox>
+          </ContentBox>
+        </>
+      )}
     </Wrap>
   );
 }
